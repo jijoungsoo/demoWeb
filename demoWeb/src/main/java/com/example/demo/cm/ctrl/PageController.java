80ae84294d1350f9fd1a66a6e6d162eada143a5d
jijoungsoo <jijsx@hotmail.com>
2021-04-10 23:42:58 +0900

    gogo

--- demoWeb/src/main/java/com/example/demo/cm/ctrl/PageController.java
+++ demoWeb/src/main/java/com/example/demo/cm/ctrl/PageController.java
@@ -81,9 +81,9 @@ public class PageController {
 		model.addAttribute("debug", debug);
         model.addAttribute("req_hm", hm);
 		long end = System.currentTimeMillis();
-		log.error("start",(start));
-		log.error("end",(end));
-		log.error("걸린시간",((end-start)/1000));
+		log.error("start","start=>"+start);
+		log.error("end","end=>"+end);
+		log.error("걸린시간","걸린시간=>"+((end-start)/1000));
 
 		
 		return "pageRouter"; 
