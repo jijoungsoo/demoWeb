105a3c582d7efc3d7bf1804084d9c7f1a62cfc63
jijoungsoo <jijsx@hotmail.com>
2021-04-15 20:23:53 +0900

    gogo

--- demoWeb/src/main/java/com/example/demo/WebConfig.java
+++ demoWeb/src/main/java/com/example/demo/WebConfig.java
@@ -17,7 +17,8 @@ public class WebConfig  implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // TODO Auto-generated method stub
         WebMvcConfigurer.super.addCorsMappings(registry);
-        registry.addMapping("/**").allowedOriginPatterns("http://localhost:8090");
+        //registry.addMapping("/**").allowedOriginPatterns("http://localhost:8090");
+        registry.addMapping("/**");
         
     }
 
