82d1d6d35811207c00048dca2712638ddb0db01f
jijoungsoo <jijsx@hotmail.com>
2021-06-13 22:54:09 +0900

    gogo

--- demoWeb/src/main/java/com/example/demo/DemoApplication.java
+++ demoWeb/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
